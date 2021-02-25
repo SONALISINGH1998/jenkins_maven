fdebd21f737b7ae58a2d1c48b86e13526902191b
SONALISINGH1998 <sonalisingh4798@gmail.com>
2021-02-24 21:53:18 -0800

    new commit

---
+++ jenkins_maven/src/main/java/com/cts/jenkins/job1/jenkins_maven/App.java
@@ -0,0 +1,13 @@
+package com.cts.jenkins.job1.jenkins_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
